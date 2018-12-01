f2c11164f9bc685809b9cf5461ac2a9f904aaa4d
Monica <monicaivan25@gmail.com>
2018-12-01 19:59:49 +0200

    Added script to Generate Button
    finished prototype

---
+++ Assets/ColorPanelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPanelScript : MonoBehaviour {
+
+
+    public GameObject ColorPanel;
+    public GameObject SaturationValue;
+    public GameObject Hue;
+
+    public void TogglePanel()
+    {
+        if(ColorPanel != null)
+        {
+            bool isActive = ColorPanel.activeSelf;
+            ColorPanel.SetActive(!isActive);
+            SaturationValue.SetActive(!isActive);
+            Hue.SetActive(!isActive);
+        }
+    }
+}
---
+++ Assets/GenerateButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateButtonScript : MonoBehaviour {
+
+    public GameObject Panel;
+
+    public void TogglePanel()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
