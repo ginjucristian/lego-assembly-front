fd3a2bf27a66f82a42d9a2f00264c244df9cacd5
Rafael <rafaelmartonca2@gmail.com>
2018-12-02 22:13:51 +0200

    Prezentarea solutiei finale prin caderea pieselor de sus (mock)

---
+++ Assets/cub_spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cub_spawn : MonoBehaviour
+{
+    
+    // Use this for initialization
+    IEnumerator Start(){
+        int x, y, z;
+        y = 5; z = -5;
+        for (x = -4; x <= 4; x = x + 2)
+        {
+            yield return new WaitForSeconds(1);
+            cub(x, y, z);
+        }
+    }
+    
+
+    void cub(int x, int y, int z)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(x, y, z);
+        cube.AddComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
