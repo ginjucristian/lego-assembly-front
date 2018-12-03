bb846f89e931683f4bbefbcb4123123a757e47cb
Monica Ivn <monicaivan25@gmail.com>
2018-12-03 11:09:10 +0200

    Added interface code, hopefully works this time

---
+++ Assets/__Scripts/ColourPanelScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourPanelScript : MonoBehaviour {
+
+    public GameObject ColourPanel;
+    public GameObject SaturationValue;
+    public GameObject Hue;
+
+    public void TogglePanel()
+    {
+        ColourPanel.SetActive(!ColourPanel.activeSelf);
+        SaturationValue.SetActive(!SaturationValue.activeSelf);
+        Hue.SetActive(!Hue.activeSelf);
+    }
+
+}
