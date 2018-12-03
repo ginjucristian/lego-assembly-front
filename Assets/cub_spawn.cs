7a9b7a674884ded44d328553cbcbac01088382f7
Rafael <rafaelmartonca2@gmail.com>
2018-12-03 20:16:52 +0200

    Modificare spawn cube.

--- Assets/cub_spawn.cs
+++ Assets/cub_spawn.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class cub_spawn : MonoBehaviour
 {
-    
+    /// <summary>
+    /// ok=1 pentru forward speed=2
+    /// ok=2 pentru cazut cuburi normal speed=5
+    /// ok=3 pentru toend speed=0
+    /// </summary>
+    int ok = 3;
+    int speed = 0;
     // Use this for initialization
     IEnumerator Start(){
+      
         int x, y, z;
         y = 5; z = -5;
-        for (x = -4; x <= 4; x = x + 2)
-        {
-            yield return new WaitForSeconds(1);
-            cub(x, y, z);
-        }
+        
+    for (x = -4; x <= 4; x = x + 2)
+    {
+        
+        if(ok==1)
+            {
+                speed = 1;
+            }
+
+        if(ok==2)
+            {
+                speed = 2;
+            }
+
+        if(ok==3)
+            {
+                speed = 0;
+            }
+
+        yield return new WaitForSeconds(speed);
+        cub(x, y, z);
+    }
     }
     
 
